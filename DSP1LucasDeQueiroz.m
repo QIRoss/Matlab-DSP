b1d6e4f2b673d0436f05f4d092769712d4f6ca83
Lucas de Queiroz <63605898+QIRoss@users.noreply.github.com>
2022-06-14 21:48:06 -0300

    3 of 3 done, still need review and conclusions

--- DSP1LucasDeQueiroz.m
+++ DSP1LucasDeQueiroz.m
@@ -6,13 +6,18 @@
 sax_5seconds_sample     = saxY(1:220500);
 voice_5seconds_sample   = voiceY(1:240000);
 
+resampled_16khz_sax   = resample(sax_5seconds_sample, 160, 441);
+resampled_16khz_voice = resample(voice_5seconds_sample, 1, 3);
+
 resampled_8khz_sax   = resample(sax_5seconds_sample, 80, 441);
 resampled_8khz_voice = resample(voice_5seconds_sample, 1, 6);
 
 %soundsc(voice_5seconds_sample, voiceFs);
+%soundsc(resampled_16khz_voice, 16000);
 %soundsc(resampled_8khz_voice, 8000);
 
 %soundsc(sax_5seconds_sample, saxFs);
+%soundsc(resampled_16khz_sax, 16000);
 %soundsc(resampled_8khz_sax, 8000);
 
 resampled_4khz_sax   = resample(sax_5seconds_sample, 40, 441);
@@ -26,9 +31,9 @@ resampled_4khz_voice = resample(voice_5seconds_sample, 1, 12);
 
 % voice plots
 
-t_voice_5s = linspace(0, length(voice_5seconds_sample)/voiceFs, length(voice_5seconds_sample) );
+t_voice_16k = linspace(0, length(resampled_16khz_voice)/16000, length(resampled_16khz_voice) );
 figure;
-plot(t_voice_5s,voice_5seconds_sample);
+plot(t_voice_16k,resampled_16khz_voice);
 title('time domain');
 xlabel('time');
 ylabel('amplitude');
@@ -49,9 +54,9 @@ ylabel('amplitude');
 
 % sax plots
 
-t_sax_5s = linspace(0, length(sax_5seconds_sample)/saxFs, length(sax_5seconds_sample) );
+t_sax_16k = linspace(0, length(resampled_16khz_sax)/16000, length(resampled_16khz_sax) );
 figure;
-plot(t_sax_5s, sax_5seconds_sample);
+plot(t_sax_16k, resampled_16khz_sax);
 title('time domain');
 xlabel('time');
 ylabel('amplitude');
@@ -72,3 +77,65 @@ ylabel('amplitude');
 
 % spectogram
 
+S = spectrogram(resampled_16khz_sax, 512);
+figure,mesh(abs(S));
+
+K = spectrogram(resampled_16khz_voice, 512);
+figure,mesh(abs(K));
+
+% SNR
+
+voice_variance  = var(resampled_16khz_voice);
+sax_variance    = var(resampled_16khz_sax);
+
+voice_with_noise_0db = resampled_16khz_voice + sqrt(voice_variance)*randn(size(resampled_16khz_voice));
+sax_with_noise_0db   = resampled_16khz_sax + sqrt(sax_variance)*randn(size(resampled_16khz_sax));
+
+%soundsc(voice_with_noise_0db, 16000);
+%soundsc(sax_with_noise_0db , 16000);
+
+voice_with_noise_10db = resampled_16khz_voice + sqrt(voice_variance/10)*randn(size(resampled_16khz_voice));
+sax_with_noise_10db   = resampled_16khz_sax + sqrt(sax_variance/10)*randn(size(resampled_16khz_sax));
+
+%soundsc(voice_with_noise_10db, 16000);
+%soundsc(sax_with_noise_10db , 16000);
+
+t_voice_noise_0db = linspace(0, length(resampled_16khz_voice)/16000, length(resampled_16khz_voice) );
+figure;
+plot(t_voice_noise_0db,voice_with_noise_0db);
+title('voice with noise 0db time domain');
+xlabel('time');
+ylabel('amplitude');
+
+t_voice_noise_10db = linspace(0, length(resampled_16khz_voice)/16000, length(resampled_16khz_voice) );
+figure;
+plot(t_voice_noise_10db,voice_with_noise_10db);
+title('voice with noise 10db time domain');
+xlabel('time');
+ylabel('amplitude');
+
+t_sax_noise_0db = linspace(0, length(resampled_16khz_voice)/16000, length(resampled_16khz_voice) );
+figure;
+plot(t_sax_noise_0db,sax_with_noise_0db);
+title('sax with noise 0db time domain');
+xlabel('time');
+ylabel('amplitude');
+
+t_sax_noise_10db = linspace(0, length(resampled_16khz_voice)/16000, length(resampled_16khz_voice) );
+figure;
+plot(t_sax_noise_10db,sax_with_noise_10db);
+title('sax with noise 10db time domain');
+xlabel('time');
+ylabel('amplitude');
+
+voice0db = spectrogram(voice_with_noise_0db, 512);
+figure,mesh(abs(voice0db));
+
+voice10db = spectrogram(voice_with_noise_10db, 512);
+figure,mesh(abs(voice10db));
+
+sax0db = spectrogram(sax_with_noise_0db, 512);
+figure,mesh(abs(sax0db));
+
+sax10db = spectrogram(sax_with_noise_10db, 512);
+figure,mesh(abs(sax10db));
